19e28e4a2de9ac87351569e31b7d42990915d55a
jef08 <jasfielding@gmail.com>
2026-04-20 18:20:26 +0200

    Added ability for freelancers to see what listings they've applied for.

--- src/Controller/FreelancerDashboardController.php
+++ src/Controller/FreelancerDashboardController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ApplicationRepository;
 
 final class FreelancerDashboardController extends AbstractController
 {
@@ -15,6 +16,20 @@ final class FreelancerDashboardController extends AbstractController
         
         return $this->render('freelancer_dashboard/index.html.twig', [
             'controller_name' => 'FreelancerDashboardController',
+        ]);
+    }
+
+    #[Route('/dashboard/applications', name: 'app_freelancer_dashboard')]
+    public function freelancerDashboard(ApplicationRepository $applicationRepository): Response {
+        $this->denyAccessUnlessGranted('ROLE_FREELANCER');
+
+        $applications = $applicationRepository->findBy(
+            ['user' => $this->getUser()],
+            ['createdAt' => 'DESC']
+        );
+
+        return $this->render('freelancer_dashboard/applications.html.twig', [
+            'applications' => $applications,
         ]);
     }
 }
